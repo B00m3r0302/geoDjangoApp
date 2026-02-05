d4dae2e27c5aa6456694722e6e919445f52d4588
B00m3r0302 <hudson.t.woomer@gmail.com>
2026-02-05 10:41:24 -0500

    done

---
+++ nearbyshops/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
+
+latitde = 39.290440
+longitude = -76.612330
+
+user_location = Point(longitude, latitde, srid=4326)
+
+# Create your views here.
+class Home(generic.ListView):
+    model = Shop
+    context_object_name = 'Shops'
+    queryset = Shop.objects.annotate(distance=Distance('location', user_location)).order_by('distance')[0:6]
+    template_name = 'shops/index.html'
--- shops/settings.py
+++ shops/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "django.contrib.gis"
+    "django.contrib.gis",
+    
+    # Local Apps
+    "nearbyshops"
 ]
 
 MIDDLEWARE = [
